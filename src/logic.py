cb8a1469281efe3882dec20b8008235a05cc3704
Illya Logvynenko <ilyamyrakami@gmail.com>
2026-03-10 13:48:02 +0200

    Improve logic: handle case and spaces

--- src/logic.py
+++ src/logic.py
@@ -1,2 +1,4 @@
 def is_anagram(text_one, text_two):
-    return sorted(text_one) == sorted(text_two)
+    clean_one = str(text_one).replace(" ", "").lower()
+    clean_two = str(text_two).replace(" ", "").lower()
+    return sorted(clean_one) == sorted(clean_two)
