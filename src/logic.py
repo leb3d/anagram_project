95b63b49a03765f224922fe066762e96651896ed
Illya Logvynenko <ilyamyrakami@gmail.com>
2026-03-10 13:56:39 +0200

    Simplify is_anagram function
    Removed functionality to ignore spaces and case sensitivity.

--- src/logic.py
+++ src/logic.py
@@ -1,5 +1,2 @@
 def is_anagram(text_one, text_two):
-    # Додаткова функціональність: ігнорування пробілів та регістру
-    clean_one = str(text_one).replace(" ", "").lower()
-    clean_two = str(text_two).replace(" ", "").lower()
-    return sorted(clean_one) == sorted(clean_two)
+    return sorted(text_one) == sorted(text_two)
