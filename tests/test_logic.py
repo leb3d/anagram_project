767c5711e4c6b5b415a22a72a5a52230fd200853
Illya Logvynenko <ilyamyrakami@gmail.com>
2026-03-10 13:47:06 +0200

    Add unit tests for is_anagram function

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,1 +1,13 @@
+from src.logic import is_anagram
 
+
+def test_positive_anagram():
+    assert is_anagram("listen", "silent") is True
+
+
+def test_negative_anagram():
+    assert is_anagram("hello", "world") is False
+
+
+def test_empty_strings():
+    assert is_anagram("", "") is True
